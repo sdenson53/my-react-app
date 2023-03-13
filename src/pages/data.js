d6c9c2501a79af6759a5b4dd710d2c571e2650da
Stephen Denson <stephen.denson@mavenwave.com>
2023-03-13 13:16:24 +0530

    first commit

---
+++ src/pages/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://imgur.com/a/Om05Jbo",
+        title: "Summer Sale",
+        desc: "flat 50% off",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://imgur.com/a/Om05Jbo",
+        title: "Spring Sale",
+        desc: "flat 50% off",
+        bg: "f5fafd"
+    },
+    {
+        id: 3,
+        img: "https://imgur.com/a/Om05Jbo",
+        title: "Winter Sale",
+        desc: "flat 50% off",
+        bg: "f5fafd"
+    }
+]
